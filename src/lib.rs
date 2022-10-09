175d7d1ec51935225b9e74b9c5483089271b9966
Gwilym Kuiper <email@gwilym.dev>
2022-10-09 14:08:33 +0100

    Can do basic initialisation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,42 @@
-use std::path::Path;
+use std::{
+    fmt::Debug,
+    path::Path,
+    sync::{Arc, RwLock},
+};
+
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 
 use tantivy::{
     directory::{error, FileHandle, WatchCallback, WatchHandle, WritePtr},
     Directory,
 };
 
-#[derive(Debug, Clone)]
-pub struct TantivySqliteStorage {}
+#[derive(Debug)]
+pub struct InitialisationError;
+
+#[derive(Clone)]
+pub struct TantivySqliteStorage {
+    inner: Arc<RwLock<TantivySqliteStorageInner>>,
+}
+
+impl Debug for TantivySqliteStorage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "TantivySqliteStorage")
+    }
+}
+
+impl TantivySqliteStorage {
+    pub fn new(
+        connection_pool: Pool<SqliteConnectionManager>,
+    ) -> Result<Self, InitialisationError> {
+        Ok(Self {
+            inner: Arc::new(RwLock::new(TantivySqliteStorageInner::new(
+                connection_pool,
+            )?)),
+        })
+    }
+}
 
 impl Directory for TantivySqliteStorage {
     fn get_file_handle(&self, path: &Path) -> Result<Box<dyn FileHandle>, error::OpenReadError> {
@@ -39,5 +69,63 @@ impl Directory for TantivySqliteStorage {
 
     fn watch(&self, watch_callback: WatchCallback) -> tantivy::Result<WatchHandle> {
         todo!()
+    }
+}
+
+struct TantivySqliteStorageInner {
+    connection_pool: Pool<SqliteConnectionManager>,
+}
+
+const INIT_SQL: &str = "
+BEGIN;
+CREATE TABLE IF NOT EXISTS tantivy_blobs (filename TEXT UNIQUE NOT NULL, content BLOB NOT NULL);
+COMMIT;
+";
+
+impl TantivySqliteStorageInner {
+    pub fn new(
+        connection_pool: Pool<SqliteConnectionManager>,
+    ) -> Result<Self, InitialisationError> {
+        let ret = Self { connection_pool };
+
+        ret.init()?;
+        Ok(ret)
+    }
+
+    fn init(&self) -> Result<(), InitialisationError> {
+        let conn = self
+            .connection_pool
+            .get()
+            .map_err(|_| InitialisationError)?;
+
+        conn.execute_batch(INIT_SQL)
+            .map_err(|_| InitialisationError)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use uuid::Uuid;
+
+    fn create_in_memory_database_string() -> String {
+        format!("file:{}?mode=memory&cache=shared", Uuid::new_v4())
+    }
+
+    fn in_memory_connection_manager() -> SqliteConnectionManager {
+        // see https://github.com/ivanceras/r2d2-sqlite/pull/45
+        SqliteConnectionManager::file(create_in_memory_database_string())
+    }
+
+    #[test]
+    fn can_successfully_init() -> Result<(), InitialisationError> {
+        let manager = in_memory_connection_manager();
+
+        let pool = Pool::builder().max_size(4).build(manager).unwrap();
+
+        let _storage = TantivySqliteStorage::new(pool)?;
+
+        Ok(())
     }
 }
