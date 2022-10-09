f01e50086192d41a10a1cd4333ed82a4221cad77
Gwilym Kuiper <email@gwilym.dev>
2022-10-09 13:23:16 +0100

    Add todo! implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::path::Path;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use tantivy::{
+    directory::{error, FileHandle, WatchCallback, WatchHandle, WritePtr},
+    Directory,
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Clone)]
+pub struct TantivySqliteStorage {}
+
+impl Directory for TantivySqliteStorage {
+    fn get_file_handle(&self, path: &Path) -> Result<Box<dyn FileHandle>, error::OpenReadError> {
+        todo!()
+    }
+
+    fn delete(&self, path: &Path) -> Result<(), error::DeleteError> {
+        todo!()
+    }
+
+    fn exists(&self, path: &Path) -> Result<bool, error::OpenReadError> {
+        todo!()
+    }
+
+    fn open_write(&self, path: &Path) -> Result<WritePtr, error::OpenWriteError> {
+        todo!()
+    }
+
+    fn atomic_read(&self, path: &Path) -> Result<Vec<u8>, error::OpenReadError> {
+        todo!()
+    }
+
+    fn atomic_write(&self, path: &Path, data: &[u8]) -> std::io::Result<()> {
+        todo!()
+    }
+
+    fn sync_directory(&self) -> std::io::Result<()> {
+        todo!()
+    }
+
+    fn watch(&self, watch_callback: WatchCallback) -> tantivy::Result<WatchHandle> {
+        todo!()
     }
 }
